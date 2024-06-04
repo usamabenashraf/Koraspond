c69bd16bdcd957d0f2b6e797574962172eebe599
usamabenashraf <84629673+usamabenashraf@users.noreply.github.com>
2024-06-04 16:15:13 +0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'], // Exclude only the "node_modules" directory
   rules: {
     // You can add or adjust rules here to fit your project's needs
   },
