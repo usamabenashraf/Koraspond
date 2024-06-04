61cac0f5d140ac45af9702a8e204b4bb09d5dd23
usamabenashraf <84629673+usamabenashraf@users.noreply.github.com>
2024-06-04 15:43:31 +0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  env: {
-    node: true, // Define your environment (e.g., Node.js)
+  languageOptions: {
+    globals: {
+      Node: true, // Define your global variables here (e.g., Node for Node.js)
+    },
   },
   extends: ['standard'], // Extend from the ESLint Standard preset
   rules: {
