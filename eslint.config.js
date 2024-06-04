13f7faf06e9609af84cd4dd151295921e0e6b748
usamabenashraf <84629673+usamabenashraf@users.noreply.github.com>
2024-06-04 15:48:39 +0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ module.exports = {
       Node: true, // Define your global variables here (e.g., Node for Node.js)
     },
   },
-  extends: ['standard'], // Extend from the ESLint Standard preset
   rules: {
     'no-undef': 'error', // Add the no-undef rule with severity 'error'
   },
