0732588ef2ffad58a77fb615d3679cf18d3b0d75
usamabenashraf <84629673+usamabenashraf@users.noreply.github.com>
2024-06-05 12:56:40 +0500

    Delete eslint.config.js

--- eslint.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/node_modules/'], // Exclude only the "node_modules" directory
-  rules: {
-    // You can add or adjust rules here to fit your project's needs
-  },
-};
