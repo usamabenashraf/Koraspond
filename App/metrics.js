6cc6ddf74644397ed0d02fed88cb32e87e99acde
usamabenashraf <84629673+usamabenashraf@users.noreply.github.com>
2024-06-05 21:54:02 +0500

    Update metrics.js

--- App/metrics.js
+++ App/metrics.js
@@ -9,9 +9,21 @@ const httpRequestCounter = new prometheus.Counter({
   labelNames: ['method', 'status'],
 });
 
-router.get('/metrics', (req, res) => {
+router.use((req, res, next) => {
+  res.on('finish', () => {
+    httpRequestCounter.labels(req.method, res.statusCode).inc();
+  });
+  next();
+});
+
+router.get('/metrics', async (req, res) => {
   res.set('Content-Type', prometheus.register.contentType);
-  res.end(prometheus.register.metrics());
+  try {
+    const metrics = await prometheus.register.metrics();
+    res.end(metrics);
+  } catch (err) {
+    res.status(500).end(err.message);
+  }
 });
 
 module.exports = router;
