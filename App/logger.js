50ac692f5c33d00b217e262f31591e1867ed7bd7
usamabenashraf <84629673+usamabenashraf@users.noreply.github.com>
2024-06-05 20:48:02 +0500

    Update logger.js

--- App/logger.js
+++ App/logger.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const fluentLogger = require('fluent-logger');
+
+// Configure Fluentd logger
+fluentLogger.configure('yourTag', {
+  host: 'yourFluentdHost',
+  port: 24224, // Default Fluentd port
+  timeout: 3.0,
+});
 
 router.use((req, res, next) => {
   const logMessage = `${req.method} ${req.url}`;
-  console.log(logMessage); // Just for demonstration
+  fluentLogger.emit('http_requests', { message: logMessage });
   next();
 });
 
